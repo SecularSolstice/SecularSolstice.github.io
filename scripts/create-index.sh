c1ffc054c8eefcc02a8394698b4cea22cbaecec5
Daniel Speyer <dspeyer@gmail.com>
2017-12-16 16:32:19 -0500

    index script

---
+++ scripts/create-index.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+TITLE="$(basename $(pwd) | tr '_' ' ')"
+
+cat <<EOF
+<html>
+  <head>
+    <title>$TITLE</title>
+    <link rel=stylesheet href=../../theme.css>
+  </head>
+  <body>
+EOF
+
+pandoc -f markdown README.md
+
+echo "<h2>Lyrics</h2><p>"
+
+cat gen/*lyrics.txt |
+    sed 's/&/\&amp;/g' |
+    sed 's/</\&lt;/g' |    
+    awk '{print $0 "<br>"}' |
+    sed 's/^   /\&nbsp; \&nbsp; \&nbsp; /'
+
+echo "</p>"
+
+echo "<h2>Files</h2><ul>"
+FILES=$( ls gen/*.pdf gen/*.midi gen/*.html gen/*.mp3 |
+               grep -v index |
+               sed 's@gen/@@' |
+               sort)
+for f in $FILES; do
+    echo "<li><a href=$f>$f</a>"
+done
+for f in chart.csv sheet-music.ly; do
+    if [ -e $f ]; then
+        echo "<li><a href=../$f>$f</a>"
+    fi
+done
+
+echo "</ul>"
+
+echo "<a href=https://github.com/SecularSolstice/SecularSolstice.github.io/tree/master/$(basename $(pwd))>Raw Git Folder</a>"
+
+echo "</body></html>"
