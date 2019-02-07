b14d38611ec043096ad2afac0a933a6e461a3349
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-02-07 18:09:11 -0500

    Add meta tags to head of HTML

--- scripts/create-main-index.sh
+++ scripts/create-main-index.sh
@@ -7,6 +7,8 @@ cat <<EOF
   <head>
     <title>Secular Solstice Resources</title>
     <link rel=stylesheet href=theme.css>
+    <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width initial-scale=1.0">
   </head>
   <body>
     <h1>Secular Solstice Resources</h1>
