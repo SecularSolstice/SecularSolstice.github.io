16ad75f7faf34b7f3288cd3779fc61173b11792b
Daniel Speyer <dspeyer@gmail.com>
2020-04-14 19:56:03 -0400

    format cookie

--- theme-switcher.js
+++ theme-switcher.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     var is_light;
     
-    if (window.location.search == 'dark') is_light=false;
-    else if (window.location.search == 'light') is_light=true;
+    if (window.location.search == 'theme=dark') is_light=false;
+    else if (window.location.search == 'theme=light') is_light=true;
     else if (document.cookie == 'dark') is_light=false;
     else if (document.cookie == 'light') is_light=true;
     else is_light=false;
@@ -16,7 +16,7 @@ window.onload = function() {
     toggle.innerText = 'color';
     toggle.onclick = function() {
         is_light = !is_light;
-        document.cookie = is_light?'light':'dark';
+        document.cookie = 'theme='+(is_light?'light':'dark');
         document.getElementsByTagName('html')[0].className = is_light?'light':'';
     };
     document.getElementsByTagName('body')[0].append(toggle);
